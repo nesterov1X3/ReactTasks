6c74e77d56f61b83279a992c8108f73e261a422c
nesterov1X3 <nesterovsergei99999@gmail.com>
2020-06-22 18:23:40 +0300

    test

--- lesson1/task3/src/index.js
+++ lesson1/task3/src/index.js
@@ -3,32 +3,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles.css'
 
-// const rootElement = document.querySelector('#root');
-
-// const element = (
-//     <div className="greeting">
-//         <div className="greeting__title">Hello, world!</div>
-//         <div className="greeting__text">I'm learning React</div>
-//     </div>
-// );
-
-// ReactDOM.render(element, rootElement);
 const rootElement = document.querySelector('#root');
-const renderGreeting = elem => {
-    const containerElem = document.createElement('div');
-    containerElem.classList.add('greeting');
-    
-    const titleElem = document.createElement('h1');
-    titleElem.classList.add('greeting__title');
-    titleElem.textContent = 'Hello, world!'; 
-    
-    const textElem = document.createElement('p');
-    textElem.classList.add('greeting__text');
-    textElem.textContent = 'I\'m learning React';
 
-    containerElem.append(titleElem, textElem);
+const element = (
+    <div className="greeting">
+        <div className="greeting__title">Hello, world!</div>
+        <div className="greeting__text">I'm learning React</div>
+    </div>
+);
 
-    elem.append(containerElem)
-};
-
-renderGreeting(rootElement);
+ReactDOM.render(element, rootElement);
