300d917bea50a2f859c97f79e515d5fe554fc1be
nesterov1X3 <nesterovsergei99999@gmail.com>
2020-06-22 18:11:52 +0300

    test

--- lesson1/task2/index.js
+++ lesson1/task2/index.js
@@ -26,12 +26,12 @@ const element = React.createElement(
     React.createElement(
         'div',
         { className: 'greeting__title' },
-        'Hello, React!'),
+        'Hello, world!'),
 
     React.createElement(
         'div',
         { className: 'greeting__text' },
         'I\'m learning React'
-    )
-)
+    ),
+);
 ReactDOM.render(element, rootElement)
