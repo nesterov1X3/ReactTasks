bccc251a92a425e59504fe5f44d94c4a60c22b31
nesterov1X3 <nesterovsergei99999@gmail.com>
2020-06-23 22:44:08 +0300

    test

--- lesson3/task2/webpack.config.js
+++ lesson3/task2/webpack.config.js
@@ -2,13 +2,15 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const webpack = require("webpack");
+const path = require('path');
+
 
 module.exports = (env, argv) => {
     const isProduction = argv.mode === "production";
     const config = {
         entry: "./src/index.jsx",
         output: {
-            filename: "bundle.js"
+            path: path.resolve(__dirname, 'review_build')
         },
         module: {
             rules: [
