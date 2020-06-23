8c7a62483b6b287d3ced5dc919c8696bd531f35f
nesterov1X3 <nesterovsergei99999@gmail.com>
2020-06-23 22:41:10 +0300

    test

--- lesson3/task2/src/Search.jsx
+++ lesson3/task2/src/Search.jsx
@@ -1,14 +1,29 @@
-export const Search = props => {
-    console.log(props);
+import React, { Component} from 'react'
+// export const Search = props => {
+//     console.log(props);
 
-     return (
-        <div className="search">
-            <h1 className="serach__title">{`Hello, ${props.name}. What to search for you?`}</h1>
-            <div className="serach__field">
-                <input type="text" className="search__input"/>
-                <button className="search__button">Search</button>
-            </div>
-        </div>
-    )
+//      return (
+//         <div className="search">
+//             <h1 className="serach__title">{`Hello, ${props.name}. What to search for you?`}</h1>
+//             <div className="serach__field">
+//                 <input type="text" className="search__input"/>
+//                 <button className="search__button">Search</button>
+//             </div>
+//         </div>
+//     )
      
+// }
+
+export class Search extends Component {
+    render(){
+        return (
+                    <div className="search">
+                        <h1 className="serach__title">{`Hello, ${this.props.name}. What to search for you?`}</h1>
+                        <div className="serach__field">
+                            <input type="text" className="search__input"/>
+                            <button className="search__button">Search</button>
+                        </div>
+                    </div>
+                )
+    }
 }
--- lesson3/task2/src/index.jsx
+++ lesson3/task2/src/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component} from 'react'
 import ReactDOM from 'react-dom'
 import './index.scss'
 import './search.scss'
