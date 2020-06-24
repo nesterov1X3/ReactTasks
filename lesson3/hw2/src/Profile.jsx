3f3235888edec26f05ec21eab9ca3722e4845a88
nesterov1X3 <nesterovsergei99999@gmail.com>
2020-06-25 01:56:05 +0300

    test

--- lesson3/hw2/src/Profile.jsx
+++ lesson3/hw2/src/Profile.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
+import moment from 'moment';
 
 
 
 const Profile = (props) => {
 
-    const birthD = new Date(props.UserData.birthDate).format('DD MMM YY')
+    const birthD = (date) => {
+       return moment(new Date(date)).format('DD MMM YY')
+    }
 
     return (
         <>
         <div className="profile__name">{`${props.UserData.firstName} ${props.UserData.lastName}`}</div>
-        <div className="profile__birth">{` Was born ${birthD } in ${props.UserData.birthPlace}`}</div>
+        <div className="profile__birth">{` Was born ${birthD(props.UserData.birthDate) } in ${props.UserData.birthPlace}`}</div>
         </>
     )
 }
--- lesson3/hw2/src/index.jsx
+++ lesson3/hw2/src/index.jsx
@@ -5,13 +5,13 @@ import Profile from './Profile.jsx'
 
 
 const UserData= {
-    firstName="John",
-    lastName="Doe",
-    birthDate=`2003-01-01T11:11:11.819Z`,
-    birthPlace='London',
+    firstName:"John",
+    lastName:"Doe",
+    birthDate:`2003-01-01T11:11:11.819Z`,
+    birthPlace:'London',
 }
 const rootElement = document.querySelector('#root')
 ReactDOM.render(<Profile
-    UserData
+    UserData={UserData}
 
 />, rootElement)
