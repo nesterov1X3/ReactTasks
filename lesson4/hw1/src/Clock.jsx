cc1e1678cb818a06122aad891acedd9078852a4c
nesterov1X3 <nesterovsergei99999@gmail.com>
2020-06-26 14:03:15 +0300

    test

--- lesson4/hw1/src/Clock.jsx
+++ lesson4/hw1/src/Clock.jsx
@@ -21,11 +21,11 @@ class Clock extends Component {
         this.state = {
             location: props.city,
             offset: props.timeLocal,
-            time:(getTimeWithOffset(this.state.offset).Date.UTC).toLocaleTimeString('en-US')
+            time:getTimeWithOffset(props.timeLocal).toLocaleTimeString('en-US',{ timeZone: 'UTC'})
         }
         setInterval(() => {
             this.setState({
-                time:(getTimeWithOffset(this.state.offset).Date.UTC).toLocaleTimeString('en-US')
+                time:getTimeWithOffset(this.state.offset).toLocaleTimeString('en-US')
             })
         },1000)
     }
