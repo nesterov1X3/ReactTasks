ea78b390c168e2ff596d22871ce1e29eaf5d0a19
nesterov1X3 <nesterovsergei99999@gmail.com>
2020-06-29 15:44:24 +0300

    test

--- lesson5/hw1/src/Toggler.jsx
+++ lesson5/hw1/src/Toggler.jsx
@@ -1,5 +1,4 @@
-//повесить обработчик 
-// 
+
 import React from 'react';
 const RED = '#f00'
 const BLUE = '#00f'
@@ -9,39 +8,36 @@ class Toggler extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            condition: 'Off'
+            condition: 'Off',
+            color: RED
         }
     }
     setBodyColorChangeText = () => {
-        // this.setState({
-        //     condition: this.state.condition = 'On'
-        // })
-
-        if (this.condition == 'Off') {
+        if (this.state.condition == 'Off') {
             this.setState({
-                condition: this.state.condition = 'On'
-            })} else {
-                this.setState({
-                    condition: this.state.condition = 'Off'
-                })
-            }
-            
-    
-        // document.body.style.backgroundColor = color;
-
+                condition: this.state.condition = 'On',
+                // color: this.state.color = RED,
+                bodyColor: document.body.style.backgroundColor = RED,
+            })
+        } else {
+            this.setState({
+                condition: this.state.condition = 'Off',
+                bodyColor: document.body.style.backgroundColor = BLUE,
+            })
+        }
     }
 
     render() {
         return (
-            <div className="buttonClick">
+            <body>
                 <button
+                
                     onClick={() => this.setBodyColorChangeText()}
                     className="toggler">{this.state.condition}</button>
 
-            </div>
+            </body>
         )
     }
 }
 
 export default Toggler;
-
