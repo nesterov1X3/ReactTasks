54ff2eb28be14652e875cec99831bc24472dacb5
nesterov1X3 <nesterovsergei99999@gmail.com>
2020-07-07 23:15:58 +0300

    tets

--- lesson8/task1/src/Life.jsx
+++ lesson8/task1/src/Life.jsx
@@ -12,6 +12,7 @@ class Life extends Component {
 
     shouldComponentUpdate(nextProps, nextState) {
         console.log('shouldComponentUpdate(nextProps, nextState): decide to render or not to render')
+        return true
     }
 
     componentDidUpdate() {
