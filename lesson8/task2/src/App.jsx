e662b4e989ccde19427b12834ea737679dddd384
nesterov1X3 <nesterovsergei99999@gmail.com>
2020-07-08 14:07:21 +0300

    test

--- lesson8/task2/src/App.jsx
+++ lesson8/task2/src/App.jsx
@@ -1,14 +1,33 @@
 import React from 'react'
 import Clock from './Clock.jsx'
 
-const App = () => {
-    return (
-        <>
-            <Clock location="New York" offset={5} />
-            <Clock location="Kyiv" offset={2} />
-            <Clock location="London" offset={0} />
-        </>
-    );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            visible: true
+        }
+        this.toggle = this.toggle.bind(this)
+    }
+    toggle() {
+        this.setState({
+            visible: !this.state.visible
+        })
+    }
+
+    render() {
+        return (
+            <>
+                <button onClick={this.toggle}>Toggle</button>
+                <div>{this.state.visible && <Clock location="New York" offset={5} />}</div>
+                <div>{this.state.visible && <Clock location="Kyiv" offset={2} />}</div>
+                <div>{this.state.visible && <Clock location="London" offset={0} />}</div>
+                
+                
+            </>
+        );
+    }
 };
 
 export default App;
