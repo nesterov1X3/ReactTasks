51b95d0a5b08217207a3d26d925ae8f425efde95
nesterov1X3 <nesterovsergei99999@gmail.com>
2020-07-09 18:09:44 +0300

    test

--- lesson9/task2/src/App.jsx
+++ lesson9/task2/src/App.jsx
@@ -1,88 +1,13 @@
 import React from 'react';
+import UserForm from './UserForm.jsx';
 
-class UserForm extends React.Component {
-constructor(props){
-    super(props)
-    this.state = {
-        name: '',
-        student: '',
-        occupation: '',
-        about: '',
-    }
-    const { user } = this.props;
+class App extends React.Component {
+  createUser = (user) => {
+    console.log(user);
+  };
+
+  render() {
+    return <UserForm onSubmit={this.createUser}/>;
+  }
 }
-    
-
-    handleChange = event => {
-        const { name, value, checked, type } = event.target;
-
-        const val = type === 'checkbox'
-        ? checked
-        :value
-
-        this.setState({
-            [name]: val
-        })
-    };
-
-    
-
-    createUser = user => {
-        event.preventDefault()
-        console.log(this.props.user);
-        
-    }
-
-
-    render() {
-        
-        return (
-            
-                <form className="login-form" onSubmit={this.createUser}>
-                    <h1 className="form-title">Profile</h1>
-                    <div className="form-control">
-                        <label className="form-label" htmlFor="name">Name</label>
-                        <input className="form-input"
-                            value={this.state.name}
-                            type="text"
-                            id="name"
-                            name="name"
-                            onChange={this.handleChange}
-                        />
-                    </div>
-                    <div className="form-control">
-                        <label className="form-label" htmlFor="student">Student</label>
-                        <input className="form-input"
-                            value={this.state.student}
-                            type="checkbox"
-                            id="student"
-                            name="student"
-                            onChange={this.handleChange} />
-                    </div>
-                    <div className="form-control">
-                        <label className="form-label" id="occupation" htmlFor="occupation">Occupation</label>
-                        <select value={this.state.occupation}
-                            name="occupation"
-                            className="form-input"
-                            onChange={this.handleChange}>
-                            <option value="london">London</option>
-                            <option value="new-york">New York</option>
-                            <option value="coconut">Sidney</option>
-                            <option value="mango">Berlin</option>
-                        </select>
-                    </div>
-                    <div className="form-control">
-                        <label className="form-label" id="about" htmlFor="about">About</label>
-                        <textarea name="about"
-                            value={this.state.about}
-                            className="form-input"
-                            onChange={this.handleChange} />
-                    </div>
-                    <button className="submit-button" type="submit">Submit</button>
-                </form>
-            
-        );
-    }
-}
-
-export default UserForm;
+export default App;
--- lesson9/task2/src/index.jsx
+++ lesson9/task2/src/index.jsx
@@ -1,16 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import UserForm from './App.jsx';
+import UserForm from './UserForm.jsx';
 
-const user = {
-    name: 'Alex',
-    student: 'true',
-    occupation: 'selo',
-    about: 'selo-2',
-}
+
 const rootElement = document.querySelector('#root');
 
 
-ReactDOM.render(<UserForm user={user} />, rootElement)
+ReactDOM.render(<UserForm />, rootElement)
 
