6581e61e71f7f2b9fa9001f2b689b6dc85ad5c60
nesterov1X3 <nesterovsergei99999@gmail.com>
2020-07-09 19:47:31 +0300

    test

--- lesson9/task3/src/UserForm.jsx
+++ lesson9/task3/src/UserForm.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 class UserForm extends React.Component {
 
     
-    handleSubmit = event => {
+    onSubmit = event => {
         event.preventDefault();
         const formData = [...new FormData(this.formRef)].reduce(
             (acc, [name, value]) => ({...acc, [name]:value}),
@@ -12,14 +12,14 @@ class UserForm extends React.Component {
         console.log(formData)
       };
 
-     onSubmit = node => {
+     setRef = node => {
          this.formRef = node;
      }
 
     render() {
         return (
             
-                <form ref={this.onSubmit} className="login-form" onSubmit={this.handleSubmit}>
+                <form ref={this.setRef} className="login-form" onSubmit={this.onSubmit}>
                     <h1 className="form-title">Profile</h1>
                     <div className="form-control">
                         <label className="form-label" htmlFor="name">Name</label>
