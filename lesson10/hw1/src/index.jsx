38979ad4bb62f456bd4793aec799b388c1538545
nesterov1X3 <nesterovsergei99999@gmail.com>
2020-07-14 14:25:27 +0300

    test

---
+++ lesson10/hw1/src/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Calculator from './Calculator.jsx';
+
+
+const rootElement = document.querySelector('#root');
+
+
+ReactDOM.render(<Calculator />, rootElement)
+
