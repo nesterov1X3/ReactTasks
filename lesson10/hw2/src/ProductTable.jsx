86ec925964d0c3af7b18032b2d4fb9618dbd9c6e
nesterov1X3 <nesterovsergei99999@gmail.com>
2020-07-14 17:36:11 +0300

    test

--- lesson10/hw2/src/ProductTable.jsx
+++ lesson10/hw2/src/ProductTable.jsx
@@ -2,39 +2,49 @@ import React from 'react';
 import ProductRow from './ProductRow.jsx';
 import ProductCategoryRow from './ProductCategoryRow.jsx';
 
-const ProductTable = ({products}) =>  {
+const ProductTable =({filterText, inStockOnly, products }) => {
 
-      const rows = [];
-      let lastCategory = null;
-      
-      products.forEach((product) => {
-        if (product.category !== lastCategory) {
-          rows.push(
-            <ProductCategoryRow
-              category={product.category}
-              key={product.category} />
-          );
-        }
+    // const filterText = this.props.filterText;
+    // const inStockOnly = this.props.inStockOnly;
+
+    const rows = [];
+    let lastCategory = null;
+
+    products.forEach((product) => {
+      if (product.name.indexOf(filterText) === -1) {
+        return;
+      }
+      if (inStockOnly && !product.stocked) {
+        return;
+      }
+      if (product.category !== lastCategory) {
         rows.push(
-          <ProductRow
-            product={product}
-            key={product.name} />
+          <ProductCategoryRow
+            category={product.category}
+            key={product.category} />
         );
-        lastCategory = product.category;
-      });
-  
-      return (
-        <table>
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Price</th>
-            </tr>
-          </thead>
-          <tbody>{rows}</tbody>
-        </table>
+      }
+      rows.push(
+        <ProductRow
+          product={product}
+          key={product.name}
+        />
       );
-    }
+      lastCategory = product.category;
+    });
+
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>{rows}</tbody>
+      </table>
+    );
+}
 
 
 export default ProductTable;
--- lesson10/hw2/src/SearchBar.jsx
+++ lesson10/hw2/src/SearchBar.jsx
@@ -1,18 +1,41 @@
 import React from 'react'
 
-const SearchBar = () => {
-    
-      return (
-        <form>
-          <input type="text" placeholder="Search..." />
-          <p>
-            <input type="checkbox" />
-            {' '}
-            Only show products in stock
-          </p>
-        </form>
-      );
-    
+class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleFilterTextChange = this.handleFilterTextChange.bind(this);
+    this.handleInStockChange = this.handleInStockChange.bind(this);
   }
+  
+  handleFilterTextChange(e) {
+    this.props.onFilterTextChange(e.target.value);
+  }
+  
+  handleInStockChange(e) {
+    this.props.onInStockChange(e.target.checked);
+  }
+  
+  render() {
+    return (
+      <form>
+        <input
+          type="text"
+          placeholder="Search..."
+          value={this.props.filterText}
+          onChange={this.handleFilterTextChange}
+        />
+        <p>
+          <input
+            type="checkbox"
+            checked={this.props.inStockOnly}
+            onChange={this.handleInStockChange}
+          />
+          {' '}
+          Only show products in stock
+        </p>
+      </form>
+    );
+  }
+}
 
 export default SearchBar;
