f277d759a859cac33cc8a17391fce913756bdd69
nesterov1X3 <nesterovsergei99999@gmail.com>
2020-07-14 14:37:39 +0300

    test

--- lesson10/hw1/src/TemperatureInput.jsx
+++ lesson10/hw1/src/TemperatureInput.jsx
@@ -6,18 +6,10 @@ const scaleNames = {
   };
 
 class TemperatureInput extends React.Component {
-    constructor(props) {
-        super(props);
-        this.handleChange = this.handleChange.bind(this);
-    }
-
-    handleChange(e) {
-        this.props.onTemperatureChange(e.target.value);
-    }
+    handleChange = (e) => this.props.onTemperatureChange(e.target.value);
 
     render() {
-        const temperature = this.props.temperature;
-        const scale = this.props.scale;
+        const { temperature, scale } = this.props;
         return (
             <fieldset>
                 <legend>Enter temperature in {scaleNames[scale]}:</legend>
