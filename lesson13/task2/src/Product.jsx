4996dcbb3af8e18e853b7e016fc551a3f3bd6a0a
nesterov1X3 <nesterovsergei99999@gmail.com>
2020-08-12 15:32:41 +0300

    test

--- lesson13/task2/src/Product.jsx
+++ lesson13/task2/src/Product.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Product({ match }) {
     return (
         <div className="product">
-            {`Product is a ${match.params.productId}`}
+            {`${match.params.productId}`}
         </div>
     );
 };
