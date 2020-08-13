ea2fd9cad8e34e74cecb05d0029e622cb52939a3
nesterov1X3 <nesterovsergei99999@gmail.com>
2020-08-14 01:30:59 +0300

    test

---
+++ lesson14/task3/src/Dimensions.jsx
@@ -0,0 +1,69 @@
+import React, { useState, useEffect } from 'react';
+
+
+
+const Dimensions = () => {
+    const [dimensions, setDimensions] = useState({
+        width:null,
+        height: null,
+    });
+
+    
+
+    useEffect(() =>{
+        const { innerHeight, innerWidth } = window;
+        setDimensions({ width: innerWidth, height: innerHeight })
+
+        const handleResize = e => {
+            const { innerWidth, innerHeight } = e.target;
+            setDimensions({ width: innerWidth, height: innerHeight })
+        }
+        
+        window.addEventListener('resize', handleResize);
+        return () => {
+            
+        window.removeEventListener('resize', handleResize);
+        }
+    }, [])
+
+    
+    const { width, height } = dimensions;
+    return <div className="dimensions">{`${width}px - ${height}px`}</div>
+}
+
+
+// class Dimensions extends React.Component {
+//     state = {
+//         width: null,
+//         height: null
+//     };
+
+//     componentDidMount() {
+//         window.addEventListener('resize', this.handleResize);
+//         const { innerHeight, innerWidth } = window;
+//         this.setDimensions({ width: innerWidth, height: innerHeight })
+//     }
+
+//     componentWillUnmount() {
+//         window.removeEventListener('resize', this.handleResize);
+//     }
+
+//     handleResize = e => {
+//         const { innerWidth, innerHeight } = e.target;
+//         this.setDimensions({ width: innerWidth, height: innerHeight })
+//     }
+//     setDimensions = ({ width, height }) => {
+//         this.setState({
+//             width,
+//             height
+//         })
+//         document.title = `${innerWidth} x ${innerHeight}`;
+//     }
+//     render() {
+//         const { width, height } = this.state;
+//         return <div class="dimensions">{`${width}px - ${height}px`}</div>
+//     }
+// }
+
+
+export default Dimensions;
